dbbffdf300e9e598b1d02e577a944787e37faa87
firthuns <firthunsnpm@gmail.com>
2026-04-08 12:07:46 +0200

    feat: nueva libreari ng build user-widget-lib

--- projects/user-widget-lib/src/lib/components/usuario/usuario.ts
+++ projects/user-widget-lib/src/lib/components/usuario/usuario.ts
@@ -9,7 +9,7 @@ import { tap } from 'rxjs/operators';
   selector: 'uw-usuario',
   imports: [],
   templateUrl: './usuario.html',
-  styleUrl: './usuario.css',
+  styleUrl: '../../assets/styles/styles.scss',
 })
 export class Usuario {
   busqueda = input<string>('');
--- projects/user-widget-lib/src/public-api.ts
+++ projects/user-widget-lib/src/public-api.ts
@@ -2,7 +2,10 @@
  * Public API Surface of user-widget-lib
  */
 
-export * from './lib/user-widget-lib.module';
 export * from './lib/components/buscador-usuario/buscador-usuario';
-export * from './lib/components/usuario/usuario';
+export { Usuario as UsuarioComponent } from './lib/components/usuario/usuario';
 export * from './lib/services/buscador-usuario.service';
+export type { Usuario as UsuarioInterface } from './lib/interfaces/usuario.interface';
+export * from './lib/constants/usuario';
+
+
